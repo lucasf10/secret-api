6f1e7e1473219fd0c1039f826c7f21ac0ab84fa7
Lucas Silva <lucas.silva@leanonsystems.com>
2022-03-25 11:59:47 -0300

    Added default token expiration

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -3,6 +3,6 @@ import jwt from 'jsonwebtoken'
 export const generateToken = (params = {}) => {
   const { SECRET, SECRET_EXPIRATION } = process.env
   return jwt.sign(params, SECRET, {
-    expiresIn: SECRET_EXPIRATION
+    expiresIn: SECRET_EXPIRATION || 86400000
   })
 }
